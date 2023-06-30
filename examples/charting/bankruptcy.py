56e1cc7d183a6b2245138fd6cfb4862057cea162
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-06-30 10:34:11 +0200

    fix wrong timestamp

--- examples/charting/bankruptcy.py
+++ examples/charting/bankruptcy.py
@@ -7,7 +7,7 @@ from charting.transformer.avg import Avg
 from examples import fred, blp
 
 if __name__ == '__main__':
-    d1, t1 = blp.get_series(series_id='BNKRINDX Index', observation_start=20060101)
+    d1, t1 = blp.get_series(series_id='BNKRINDX Index', observation_start="20060101")
     d2, t2 = fred.get_series(series_id='JHDUSRGDPBR', observation_start="2006-01-01")
 
     chart = TimeSeriesChart(title="Bankruptcy filings moving up in recent weeks", num_y_axes=2)
@@ -15,9 +15,10 @@ if __name__ == '__main__':
     chart.configure_y_axis(axis_index=0, label="Count")
     chart.configure_y_axis(axis_index=1, label="Count")
 
+    minor_locator = mdates.YearLocator(base=1)
     major_locator = mdates.YearLocator(base=2)
     major_formatter = mdates.DateFormatter("%Y")
-    chart.configure_x_axis(major_formatter=major_formatter, major_locator=major_locator)
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
 
     chart.add_data(x=d1.index, y=d1['y'], label="Bankruptcy filings", y_axis=0,
                    transformer=Avg(offset=DateOffset(months=1)))
