4942a87a041ed3cde7035243c1d0ad7af84081c7
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-07 10:04:47 +0200

    add dev charts

---
+++ charts/development/revisions.py
@@ -0,0 +1,36 @@
+from matplotlib.ticker import FuncFormatter, MultipleLocator
+
+from charting import fred
+from charting.model.chart import Chart
+import matplotlib.dates as mdates
+
+
+def millions(x, pos):
+    return '%1.0f' % (x * 1e-3)
+
+
+if __name__ == '__main__':
+    chart = Chart(title="Revisions vs. Adjusted", num_y_axis=2,
+                  filename="revisions-vs-adjusted.png")
+
+    d0, t0 = fred.get_series(series_id='PAYEMS', observation_start="2020-01-01", observation_end="2023-06-30")
+    d1, t1 = fred.get_series(series_id='PAYEMS', realtime_start="2020-01-01", realtime_end="2023-06-30",
+                             observation_start="2020-01-01", observation_end="2023-06-30")
+    diff = d1 - d0
+
+    chart.configure_y_axis(y_axis_index=0, label="Millions of Persons", major_formatter=FuncFormatter(millions))
+    chart.configure_y_axis(y_axis_index=1, label=u'Δ')
+
+    major_locator = mdates.YearLocator(base=1)
+    minor_locator = mdates.MonthLocator(interval=6)
+    major_formatter = mdates.DateFormatter("%Y")
+
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator,
+                           major_locator=major_locator)
+
+    chart.add_series(d0.index, d0["y"], label=t0)
+    chart.add_series(d1.index, d1["y"], label=f'{t1} (Realtime)')
+    chart.add_series(diff.index, diff["y"], y_axis_index=1, label='Revisions minus Adjusted')
+
+    chart.legend()
+    chart.plot()
---
+++ charts/development/us_retail_sakes_nominal_vs_inflation_adjusted.py
@@ -0,0 +1,29 @@
+from charting import fred
+from charting.model.chart import Chart
+import matplotlib.dates as mdates
+
+from charting.transformer.pct import Pct
+
+if __name__ == '__main__':
+    chart = Chart(title="US Retail Sales - Nominal vs. Inflation Adjusted", num_rows=2, num_y_axis=1,
+                  filename="us_retail_sakes_nominal_vs_inflation_adjusted.png")
+
+    d0, t0 = fred.get_series(series_id='MRTSSM44000USS', observation_start="1992-01-01")
+
+    chart.configure_y_axis(row_index=0, y_axis_index=0)
+    chart.configure_y_axis(row_index=1, y_axis_index=0)
+    chart.add_sup_y_label(label="%")
+
+    major_locator = mdates.YearLocator(base=5)
+    minor_locator = mdates.YearLocator(base=1)
+    major_formatter = mdates.AutoDateFormatter(major_locator)
+
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator,
+                           major_locator=major_locator)
+
+    chart.add_series(d0.index, d0["y"], row_index=0, label=t0,
+                     transformer=Pct(periods=12))
+
+    chart.add_last_value_badge()
+    chart.legend(ncol=2)
+    chart.plot()
