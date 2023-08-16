2b452b20710231968327e50bd34a50ab2f2172d1
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-16 08:38:23 +0200

    add redbook research

---
+++ charts/production/us_redbook_retail_sales_yoy.py
@@ -0,0 +1,30 @@
+import matplotlib.dates as mdates
+from pandas import DateOffset
+
+from charting.model.chart import Chart
+from charting.model.metadata import Metadata, Category, Region
+from charting.transformer.avg import Avg
+from charting.transformer.pct import Pct
+from charting import blp, fred
+
+if __name__ == '__main__':
+    title = "Redbook Research: Same Store, Retails Sales Average"
+
+    d1, t1 = blp.get_series(series_id='REDSWYOY Index', observation_start="2013-11-01")
+    d2, t2 = fred.get_series(series_id='JHDUSRGDPBR')
+
+    metadata = Metadata(title=title, region=Region.US, category=Category.CONSUMER)
+    chart = Chart(title=title, metadata=metadata, filename="us_redbook_retail_sales_yoy.png")
+
+    minor_locator = mdates.MonthLocator(interval=1)
+    major_locator = mdates.MonthLocator(interval=3)
+    major_formatter = mdates.DateFormatter(fmt="%m/%Y")
+
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
+    chart.configure_y_axis(y_axis_index=0, label="%", y_lim=(-15, 25))
+
+    chart.add_series(x=d1.index, y=d1['y'], label=t1, transformer=Pct(periods=12))
+    chart.add_vertical_line(x=d2.index, y=d2["y"], label="US Recession")
+
+    chart.legend()
+    chart.plot()
