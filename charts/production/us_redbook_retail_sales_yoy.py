4fa3bec56a178b8b8ed7c46188b7e6ff6ab6d031
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-16 08:48:47 +0200

    finish redbook retail sales

--- charts/production/us_redbook_retail_sales_yoy.py
+++ charts/production/us_redbook_retail_sales_yoy.py
@@ -10,21 +10,22 @@ from charting import blp, fred
 if __name__ == '__main__':
     title = "Redbook Research: Same Store, Retails Sales Average"
 
-    d1, t1 = blp.get_series(series_id='REDSWYOY Index', observation_start="2013-11-01")
+    d1, t1 = blp.get_series(series_id='REDSWYOY Index', observation_start="20131101")
     d2, t2 = fred.get_series(series_id='JHDUSRGDPBR')
 
     metadata = Metadata(title=title, region=Region.US, category=Category.CONSUMER)
     chart = Chart(title=title, metadata=metadata, filename="us_redbook_retail_sales_yoy.png")
 
-    minor_locator = mdates.MonthLocator(interval=1)
-    major_locator = mdates.MonthLocator(interval=3)
-    major_formatter = mdates.DateFormatter(fmt="%m/%Y")
+    minor_locator = mdates.MonthLocator(interval=3)
+    major_locator = mdates.MonthLocator(interval=12)
+    major_formatter = mdates.DateFormatter(fmt="%Y")
 
     chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
-    chart.configure_y_axis(y_axis_index=0, label="%", y_lim=(-15, 25))
+    chart.configure_y_axis(y_axis_index=0, label="% YoY", y_lim=(-15, 25))
 
-    chart.add_series(x=d1.index, y=d1['y'], label=t1, transformer=Pct(periods=12))
+    chart.add_series(x=d1.index, y=d1['y'], label=t1)
     chart.add_vertical_line(x=d2.index, y=d2["y"], label="US Recession")
+    chart.add_horizontal_line()
 
     chart.legend()
     chart.plot()
