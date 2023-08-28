cad2ad27cb32d771da456ae6c8f83457088956c6
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-28 13:54:07 +0200

    add eu inflation

--- charts/production/eu_inflation_cpi_pmi.py
+++ charts/production/eu_inflation_cpi_pmi.py
@@ -1,6 +1,8 @@
+import pandas as pd
 from matplotlib.ticker import MultipleLocator
 from source_engine.sdmx_source import Bbk
 
+from charting import blp
 from charting.model.chart import Chart
 from charting.model.metadata import Region, Category, Metadata
 import matplotlib.dates as mdates
@@ -13,12 +15,11 @@ if __name__ == '__main__':
     d1['y'] = d1['y'].astype(float)
 
     title = "Eurozone inflation"
-    #metadata = Metadata(title=title, region=Region.EU, category=Category.INFLATION)
-    chart = Chart(title=title, filename="eu_inflation_cpi_pmi.png")
+    metadata = Metadata(title=title, region=Region.EU, category=Category.INFLATION)
+    chart = Chart(title=title, metadata=metadata, filename="eu_inflation_cpi_pmi.png")
 
     chart.configure_y_axis(y_axis_index=0, label="%", minor_locator=MultipleLocator(1),
                            major_locator=MultipleLocator(3))
-
     minor_locator = mdates.YearLocator(base=1)
     major_locator = mdates.YearLocator(base=3)
     major_formatter = mdates.DateFormatter("%Y")
