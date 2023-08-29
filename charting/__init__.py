514080b4507aef0d199307be4953ad7f65e479d0
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-29 10:59:44 +0200

    fix missing package data for production charts

--- charting/__init__.py
+++ charting/__init__.py
@@ -2,7 +2,7 @@
 .. include:: ../README.md
 """
 
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 
 import getpass
 import os.path
--- charting/update.py
+++ charting/update.py
@@ -3,7 +3,7 @@ import importlib
 
 
 def execute_main_methods():
-    path = os.path.join(os.path.dirname(os.getcwd()), "charts", "production")
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "charts", "production")
 
     file_list = os.listdir(path)
 
