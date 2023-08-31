d2ad7f716d8f8d3696eb0fe788355aa431c2ec81
ssymhoven <simon.symhoven@donner-reuschel.lu>
2023-08-31 09:47:54 +0200

    fix update script

--- charting/update.py
+++ charting/update.py
@@ -3,14 +3,14 @@ import importlib
 
 
 def execute_main_methods():
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "charts", "production")
+    path = os.path.join(os.path.dirname(__file__), "charts", "production")
 
     file_list = os.listdir(path)
 
     for file_name in file_list:
         if file_name.endswith('.py') and file_name != "__init__.py":
             module_name = file_name[:-3]
-            module = importlib.import_module(f'charts.production.{module_name}')
+            module = importlib.import_module(f'charting.charts.production.{module_name}')
 
             if hasattr(module, 'main') and callable(getattr(module, 'main')):
                 module.main()
