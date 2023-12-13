230be4b01fbcdb1bc1f716cb6c93476c582e7c08
orodrigues <orlando.rodrigues@donner-reuschel.lu>
2023-12-13 11:53:47 +0100

    add new charts for equity

---
+++ charting/charts/development/eu_European_Indizes_YTD.py
@@ -0,0 +1,42 @@
+import datetime
+
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator
+from source_engine.bloomberg_source import BloombergSource
+
+from charting.model.chart import Chart
+from charting.model.metadata import Metadata, Region, Category
+from charting.transformer.pct import Pct
+from charting.transformer.resample import Resample
+
+
+def main():
+    blp = BloombergSource()
+
+    df1, t1 = blp.get_series(series_id='sx5e Index', observation_start="20221231")
+    df2, t2 = blp.get_series(series_id='sxxp Index', observation_start="20221231")
+    df3, t3 = blp.get_series(series_id='mcxp Index', observation_start="20221231")
+    df4, t4 = blp.get_series(series_id='scxp Index', observation_start="20221231")
+
+    title = "European Indizes YTD"
+
+    chart = Chart(title=title, filename="european_indizes_YTD.png")
+
+    chart.configure_y_axis(y_axis_index=0, label="%")
+
+    minor_locator = mdates.MonthLocator(interval=1)
+    major_locator = mdates.MonthLocator(interval=3)
+    major_formatter = mdates.DateFormatter("%b %y")
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
+
+    chart.add_series(x=df1.index, y=df1['y'], label=t1, transformer=Pct(periods=5))
+    chart.add_series(x=df2.index, y=df2['y'], label=t2, transformer=Pct(periods=5))
+    chart.add_series(x=df3.index, y=df3['y'], label=t3, transformer=Pct(periods=5))
+    chart.add_series(x=df4.index, y=df4['y'], label=t4, transformer=Pct(periods=5))
+
+    chart.legend(ncol=2)
+    chart.plot()
+
+
+if __name__ == '__main__':
+    main()
