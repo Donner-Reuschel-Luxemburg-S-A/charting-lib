730c681b888a4dfa42b153f4ad2e4a0ab9add080
ssymhoven <simon.symhoven@donner-reuschel.lu>
2024-01-29 15:57:07 +0100

    add ig credit eu

--- charting/charts/production/eu_ig_credit_spread.py
+++ charting/charts/production/eu_ig_credit_spread.py
@@ -1,30 +1,36 @@
+import datetime
+
 import matplotlib.dates as mdates
+from dateutil.relativedelta import relativedelta
 from matplotlib.ticker import MultipleLocator
 from source_engine.bloomberg_source import BloombergSource
 
 from charting.model.chart import Chart
 from charting.model.metadata import Metadata, Region, Category
 
+
 def main():
     blp = BloombergSource()
 
-    df1, t1 = blp.get_series(series_id='LECPOAS Index', observation_start="20140101")
+    start = datetime.datetime.today().date() - relativedelta(years=10)
+
+    df1, t1 = blp.get_series(series_id='LECPOAS Index', observation_start=start.strftime("%Y%m%d"))
 
     title = "EUR Investment Grade Corporate Bond Spreads"
 
     metadata = Metadata(title=title, region=Region.EU, category=Category.FI)
     chart = Chart(title=title, filename="eu_ig_credit_spread.png", metadata=metadata)
 
-    chart.configure_y_axis(y_axis_index=0, label="Spread (BPS)", minor_locator=MultipleLocator(10),
-                           major_locator=MultipleLocator(50))
+    chart.configure_y_axis(y_axis_index=0, label="Percentage Points", minor_locator=MultipleLocator(0.1),
+                           major_locator=MultipleLocator(0.5))
 
-    minor_locator = mdates.YearLocator(base=1)
-    major_locator = mdates.YearLocator(base=2)
+    minor_locator = mdates.MonthLocator(interval=6)
+    major_locator = mdates.YearLocator(base=1)
     major_formatter = mdates.DateFormatter("%y")
     chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
-
+    mean_val = [df1['y'].mean()] * len(df1.index)
     chart.add_series(x=df1.index, y=df1['y'], label=t1)
-    chart.add_series(x=df1.index, y=df1['y'].mean(), label="Avg", linestyle="--")
+    chart.add_series(x=df1.index, y=mean_val, label="10Y Avg", linestyle="--")
 
     chart.legend(ncol=2)
     chart.plot()
