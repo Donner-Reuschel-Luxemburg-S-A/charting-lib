cc6024e772768ffbbd22cb491322d52018c9aa3c
ssymhoven <simon.symhoven@donner-reuschel.lu>
2024-02-05 13:06:33 +0100

    rework

--- charting/charts/production/us_jolts.py
+++ charting/charts/production/us_jolts.py
@@ -1,41 +1,33 @@
 import matplotlib.dates as mdates
 from matplotlib.ticker import MultipleLocator
-from pandas import DateOffset
+from source_engine.bloomberg_source import BloombergSource
 from source_engine.fred_source import FredSource
-from source_engine.indeed_source import IndeedSource
 
 from charting.model.chart import Chart
-from charting.model.metadata import Metadata, Region, Category
-from charting.transformer.lead import Lead
+from charting.model.metadata import Metadata, Category, Region
 
 
 def main():
+    blp = BloombergSource()
     fred = FredSource()
-    indeed = IndeedSource()
-    d0, t0 = fred.get_series(series_id="JTSJOL", observation_start="2019-06-01")
-    percentage_change = (d0['y'] / d0['y'].iloc[0]) * 100
 
-    d1, t1 = indeed.get_series(series_id="US")
+    start_time = "19700101"
 
-    title = "JOLTs job openings likely eased moderately"
+    jolts_df, jolts_title = blp.get_series(series_id="JOLTTOTL Index", observation_start=start_time)
+
+    us_nber_df, us_nber_title = fred.get_series(series_id='JHDUSRGDPBR', observation_start=start_time)
+
+    title = "US JOLTS Job Openings"
     metadata = Metadata(title=title, region=Region.US, category=Category.EMPLOYMENT)
 
     chart = Chart(title=title, filename="us_jolts.png", metadata=metadata)
+    chart.configure_x_axis(minor_locator=mdates.YearLocator(base=1), major_locator=mdates.YearLocator(base=5))
+    chart.configure_y_axis(minor_locator=MultipleLocator(100), major_locator=MultipleLocator(500), label="")
 
-    chart.configure_y_axis(y_axis_index=0, label="Percentage Points", minor_locator=MultipleLocator(5),
-                           major_locator=MultipleLocator(10))
+    chart.add_series(jolts_df.index, jolts_df['y'], label=jolts_title)
 
-    major_locator = mdates.MonthLocator(interval=6)
-    minor_locator = mdates.MonthLocator(interval=1)
-    major_formatter = mdates.DateFormatter("%b %y")
-
-    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator,
-                           major_locator=major_locator)
-
-    chart.add_series(d0.index, percentage_change, label=t0, transformer=Lead(offset=DateOffset(months=1)))
-    chart.add_series(d1.index, d1["indeed_job_postings_index_SA"].values, label="Indeed, Seasonally Adjusted")
-    chart.add_horizontal_line(y=100)
-
+    chart.add_vertical_line(x=us_nber_df.index, y=us_nber_df["y"], label=us_nber_title)
+    chart.add_horizontal_line(y=0)
     chart.legend(ncol=2)
     chart.plot()
 
