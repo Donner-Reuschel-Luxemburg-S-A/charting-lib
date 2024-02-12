c7b8bbfda5f59de95d557a21f981bb05e1904a06
Tilman BACHER <tbacher@donner-reuschel.lu>
2024-02-12 13:04:07 +0100

    Clean up and move to production

--- charting/charts/production/eur_corop_bb_bbb_spread.py
+++ charting/charts/production/eur_corop_bb_bbb_spread.py
@@ -14,20 +14,22 @@ def main():
     t2 = 'Bloomberg Pan-European Aggregate: Corporate Baa Total Return'
     metadata = Metadata(title=title, region=Region.EU, category=Category.CREDIT)
 
-    chart = Chart(title=title, num_rows=2,  metadata=metadata, filename=__name__ + ".png")
+    chart = Chart(title=title, num_rows=2, metadata=metadata, filename=__name__ + ".png")
     minor_locator = mdates.MonthLocator(interval=3)
     major_locator = mdates.MonthLocator(interval=12)
     major_formatter = mdates.DateFormatter("%b %y")
-    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator,major_locator=major_locator)
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
     chart.configure_y_axis(y_axis_index=0, row_index=0, label='Spread BPS', minor_locator=MultipleLocator(50),
                            major_locator=MultipleLocator(100))
-    chart.configure_y_axis(y_axis_index=0, row_index=1, label='Spread Difference BPS', minor_locator=MultipleLocator(25),
+    chart.configure_y_axis(y_axis_index=0, row_index=1, label='Spread Difference BPS',
+                           minor_locator=MultipleLocator(25),
                            major_locator=MultipleLocator(50))
     chart.add_series(x=df.index, y=df['y'], label=t)
     chart.add_series(x=df2.index, y=df2['y'], label=t2)
-    chart.add_series(x=df.index, y=(df-df2)['y'], row_index=1, label=title)
+    chart.add_series(x=df.index, y=(df - df2)['y'], row_index=1, label=title)
     chart.legend()
     chart.plot()
+
 
 if __name__ == '__main__':
     main()
--- charting/charts/production/eur_corop_bbb_covered_spread.py
+++ charting/charts/production/eur_corop_bbb_covered_spread.py
@@ -14,26 +14,29 @@ def main():
     t = 'iBoxx Euro Germany Covered Total Return Index'
     t2 = 'Bloomberg Pan-European Aggregate: Corporate Baa Total Return'
     metadata = Metadata(title=title, region=Region.EU, category=Category.CREDIT)
-#,  metadata=metadata
+
     common_index = pd.DatetimeIndex(set(df.index).intersection(set(df2.index))).sort_values()
 
-    chart = Chart(title=title, num_rows=2,num_y_axis=2, filename=__name__ + ".png")
+    chart = Chart(title=title, num_rows=2, num_y_axis=2, filename=__name__ + ".png", metadata=metadata)
     minor_locator = mdates.MonthLocator(interval=3)
     major_locator = mdates.MonthLocator(interval=12)
     major_formatter = mdates.DateFormatter("%b %y")
-    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator,major_locator=major_locator)
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
     chart.configure_y_axis(y_axis_index=0, row_index=0, label='Spread BPS', minor_locator=MultipleLocator(50),
                            major_locator=MultipleLocator(100))
     chart.configure_y_axis(y_axis_index=1, row_index=0, label='Spread BPS', minor_locator=MultipleLocator(5),
                            major_locator=MultipleLocator(10))
-    chart.configure_y_axis(y_axis_index=0, row_index=1, label='Spread Difference BPS', minor_locator=MultipleLocator(25),
+    chart.configure_y_axis(y_axis_index=0, row_index=1, label='Spread Difference BPS',
+                           minor_locator=MultipleLocator(25),
                            major_locator=MultipleLocator(50))
     chart.add_series(x=common_index, y=df.loc[common_index]['y'], label=t, y_axis_index=1)
     chart.add_series(x=common_index, y=df2.loc[common_index]['y'], label=t2, y_axis_index=0)
-    chart.add_series(x=common_index, y=(df2.loc[common_index]-df.loc[common_index])['y'], row_index=1,y_axis_index=0, label=title)
-    chart.configure_y_axis(label='',row_index=1,y_axis_index=1,major_locator=NullLocator())
+    chart.add_series(x=common_index, y=(df2.loc[common_index] - df.loc[common_index])['y'], row_index=1, y_axis_index=0,
+                     label=title)
+    chart.configure_y_axis(label='', row_index=1, y_axis_index=1, major_locator=NullLocator())
     chart.legend()
     chart.plot()
+
 
 if __name__ == '__main__':
     main()
