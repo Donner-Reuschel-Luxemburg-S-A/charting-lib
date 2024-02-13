304c87d2a11833ce6206071627d76cc393279ce0
Tilman BACHER <tbacher@donner-reuschel.lu>
2024-02-13 09:58:50 +0100

    EM vs US credit

---
+++ charting/charts/development/em_hc_us_bbb_corp.py
@@ -0,0 +1,37 @@
+from matplotlib.ticker import MultipleLocator
+from source_engine.bloomberg_source import BloombergSource
+from charting.model.chart import Chart
+from charting.model.metadata import Metadata, Region, Category
+import matplotlib.dates as mdates
+import pandas as pd
+
+
+def main():
+    blp = BloombergSource()
+    df, t = blp.get_series(series_id='LG20STAT Index', observation_start='20140101', field='BX218')
+    df2, t2 = blp.get_series(series_id='I02561EU Index', observation_start='20140101', field='BX218')
+    title = 'EM HC USD vs US Corporate BBB'
+    t = 'Bloomberg EM Hard Currency Aggregate TR Index Value Unhedged USD'
+    t2 = 'Bloomberg Pan-European Aggregate: Corporate Baa Total Return'
+    metadata = Metadata(title=title, region=Region.EU, category=Category.CREDIT)
+    common_index = pd.DatetimeIndex(set(df.index).intersection(set(df2.index))).sort_values()
+#, metadata=metadata
+    chart = Chart(title=title, num_rows=2, filename=__name__ + ".png")
+    minor_locator = mdates.MonthLocator(interval=3)
+    major_locator = mdates.MonthLocator(interval=12)
+    major_formatter = mdates.DateFormatter("%b %y")
+    chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
+    chart.configure_y_axis(y_axis_index=0, row_index=0, label='Spread BPS vs Tsy', minor_locator=MultipleLocator(50),
+                           major_locator=MultipleLocator(100))
+    chart.configure_y_axis(y_axis_index=0, row_index=1, label='Spread Difference BPS',
+                           minor_locator=MultipleLocator(25),
+                           major_locator=MultipleLocator(50))
+    chart.add_series(x=common_index, y=df.loc[common_index]['y'], label=t)
+    chart.add_series(x=common_index, y=df2.loc[common_index]['y'], label=t2)
+    chart.add_series(x=common_index, y=(df.loc[common_index] - df2.loc[common_index])['y'], row_index=1, label=title)
+    chart.legend()
+    chart.plot()
+
+
+if __name__ == '__main__':
+    main()
