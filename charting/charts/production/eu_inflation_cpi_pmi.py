1207b2c86c3cf5ac64a92d98bb263f9ea347a714
Tilman BACHER <tbacher@donner-reuschel.lu>
2024-05-17 12:23:00 +0200

    new chart + fixes

--- charting/charts/production/eu_inflation_cpi_pmi.py
+++ charting/charts/production/eu_inflation_cpi_pmi.py
@@ -1,6 +1,6 @@
 import matplotlib.dates as mdates
 from matplotlib.ticker import MultipleLocator
-from source_engine.sdmx_source import Bbk
+from source_engine.sdmx_source import Bbk, Ecb
 
 from charting.model.chart import Chart
 from charting.model.metadata import Region, Category, Metadata
@@ -9,8 +9,12 @@ from charting.transformer.pct import Pct
 
 def main():
     bbk = Bbk()
+    estat = Ecb()
     d1, t1 = bbk.get_data(flow_ref="BBXP1", key="M.U2.N.HICP.000000.IND.I00", parameters={'startPeriod': '2006-01'})
+    d2, t2 = estat.get_data(flow_ref="ICP", key="M.U2.N.XEF000.4.INX", parameters={'startPeriod': '2006-01'})
+
     d1['y'] = d1['y'].astype(float)
+    d2['y'] = d2['y'].astype(float)
 
     title = "Eurozone inflation"
     metadata = Metadata(title=title, region=Region.EU, category=Category.INFLATION)
@@ -24,8 +28,9 @@ def main():
     chart.configure_x_axis(major_formatter=major_formatter, minor_locator=minor_locator, major_locator=major_locator)
 
     chart.add_series(x=d1.index, y=d1['y'], label="Eurozone Harmonized CPI", transformer=Pct(periods=12))
-    chart.add_horizontal_line()
-
+    chart.add_series(x=d2.index, y=d2['y'], label="Eurozone Harmonized CPI ex. Energy, Food, Alcohol and Tabacco", transformer=Pct(periods=12))
+    chart.add_horizontal_line(y=2)
+    chart.add_last_value_badge()
     chart.legend()
     chart.plot()
 
--- charting/charts/production/global_wti_brent_oil.py
+++ charting/charts/production/global_wti_brent_oil.py
@@ -8,8 +8,8 @@ from charting.model.metadata import Metadata, Region, Category
 
 def main():
     fred = FredSource()
-    d1, t1 = fred.get_series(series_id='DCOILWTICO', observation_start="2020-01-01")
-    d2, t2 = fred.get_series(series_id='DCOILBRENTEU', observation_start="2020-01-01")
+    d1, t1 = fred.get_series(series_id='DCOILWTICO', observation_start="2021-01-01")
+    d2, t2 = fred.get_series(series_id='DCOILBRENTEU', observation_start="2021-01-01")
 
     title = 'WTI & Brent Oil'
     metadata = Metadata(title=title, region=Region.GLOBAL, category=Category.COMMODITY)
