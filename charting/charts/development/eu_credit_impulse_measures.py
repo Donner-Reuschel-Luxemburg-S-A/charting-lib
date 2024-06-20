2ce52d30d010c20f73394d5b64353735d432a326
Tilman BACHER <tbacher@donner-reuschel.lu>
2024-06-20 16:11:54 +0200

    split credit measures

---
+++ charting/charts/development/eu_credit_impulse_measures.py
@@ -0,0 +1,51 @@
+import matplotlib.dates as mdates
+from matplotlib.ticker import MultipleLocator
+from source_engine.bloomberg_source import BloombergSource
+
+from charting.model.chart import Chart
+from charting.model.metadata import Metadata, Category, Region
+
+
+def main(**kwargs):
+    blp = BloombergSource()
+
+    credit_corp_df, credit_corp_title = blp.get_series(series_id="MFIKNFG Index", observation_start="19990101")
+    credit_hh_df, credit_hh_title = blp.get_series(series_id="MFIPHGS Index", observation_start="19990101")
+    credit_impulse_df, credit_impulse_title = blp.get_series(series_id="BCMPCIGD Index", observation_start="19990101")
+    credit_impulse_hh_df, credit_impulse_hh_title = blp.get_series(series_id="BCMPCIHN Index",
+                                                                   observation_start="19990101")
+    credit_impulse_ps_df, credit_impulse_ps_title = blp.get_series(series_id="BCMPCIPD Index",
+                                                                   observation_start="19990101")
+
+    title = "EU Credit Measures YoY"
+    metadata = Metadata(title=title, region=Region.DE, category=Category.INFLATION)
+
+    chart = Chart(title=title, metadata=metadata, filename="eu_credit_measures_yoy.png")
+    chart.configure_x_axis(minor_locator=mdates.YearLocator(base=1), major_locator=mdates.YearLocator(base=5))
+    chart.configure_y_axis(minor_locator=MultipleLocator(1), major_locator=MultipleLocator(5), label="%")
+
+    chart.add_series(credit_corp_df.index, credit_corp_df['y'], label=credit_corp_title)
+    chart.add_series(credit_hh_df.index, credit_hh_df['y'], label=credit_hh_title)
+
+    chart.add_horizontal_line()
+    chart.legend(ncol=1)
+    return chart.plot(upload_chart='observation_start' not in kwargs)
+
+    title = "EU Credit Impulse"
+    metadata = Metadata(title=title, region=Region.DE, category=Category.INFLATION)
+
+    chart = Chart(title=title, metadata=metadata, filename="eu_credit_impulse_measures_yoy.png")
+    chart.configure_x_axis(minor_locator=mdates.YearLocator(base=1), major_locator=mdates.YearLocator(base=5))
+    chart.configure_y_axis(minor_locator=MultipleLocator(1), major_locator=MultipleLocator(5), label="%")
+
+    chart.add_series(credit_impulse_df.index, credit_impulse_df['y'], label=credit_impulse_title)
+    chart.add_series(credit_impulse_hh_df.index, credit_impulse_hh_df['y'], label=credit_impulse_hh_title)
+    chart.add_series(credit_impulse_ps_df.index, credit_impulse_ps_df['y'], label=credit_impulse_ps_title)
+
+    chart.add_horizontal_line()
+    chart.legend(ncol=1)
+    return chart.plot(upload_chart='observation_start' not in kwargs)
+
+
+if __name__ == '__main__':
+    main()
