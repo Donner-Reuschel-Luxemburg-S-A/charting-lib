e81b0c359fbc76689997dac20045e3fe8f756f7c
tbacher <tilman.bacher@donner-reuschel.lu>
2024-09-23 12:10:18 +0200

    new chart on just 10y breaks

---
+++ charting/charts/production/de_breaks_10y.py
@@ -0,0 +1,38 @@
+import datetime
+
+import matplotlib.dates as mdates
+from source_engine.bloomberg_source import BloombergSource
+
+from charting.model.chart import Chart
+from charting.model.metadata import Metadata, Region, Category
+
+DEFAULT_START_DATE = datetime.date(2018, 1, 1)
+DEFAULT_END_DATE = datetime.datetime.today()
+
+
+def main(**kwargs):
+    observation_start = kwargs.get('observation_start', DEFAULT_START_DATE)
+    observation_end = kwargs.get('observation_end', DEFAULT_END_DATE)
+
+    blp = BloombergSource()
+    df1, t1 = blp.get_series(series_id='DEGGBE10 Index', observation_start=observation_start.strftime("%Y%m%d"),
+                             observation_end=observation_end.strftime("%Y%m%d"), field='PX_LAST')
+
+    title = "Germany Breakeven Rates"
+    metadata = Metadata(title=title, region=Region.DE, category=Category.RATES)
+    chart = Chart(title=title, metadata=metadata, filename="de_breaks_10y", language=kwargs.get('language', 'en'))
+
+    chart.configure_y_axis(label="BASISPOINTS")
+    chart.configure_x_axis(major_formatter=mdates.DateFormatter("%b %y"))
+
+    chart.add_series(x=df1.index, y=df1['y'], label="Deutschland Inflationserwartungen")
+
+    chart.add_last_value_badge(decimals=2)
+
+    chart.legend(ncol=1)
+    return chart.plot(upload_chart='observation_start' not in kwargs)
+
+
+if __name__ == '__main__':
+    main(language='en')
+    main(language='de')
